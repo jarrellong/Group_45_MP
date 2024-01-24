433e77da1513e8714333088b842a3cfd10787393
ZeroSighs <liewchernyu04@gmail.com>
2024-01-24 17:46:29 +0800

    sadmka;lkje

--- Simple/app.js
+++ Simple/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 const connection = mysql.createConnection({
   host: 'localhost',
