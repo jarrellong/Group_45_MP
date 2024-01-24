5a191de963899ed2e69011c6aec2141d6ceb4dc6
ZeroSighs <liewchernyu04@gmail.com>
2024-01-24 16:29:37 +0800

    mp

--- Simple/public/app.js
+++ Simple/public/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -18,16 +20,33 @@ connection.connect((err) => {
   console.log('Connected to MySQL as id ' + connection.threadId);
 });
 
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
-  // Query the database to test connection
-  connection.query('SELECT 1', (error, results, fields) => {
-    if (error) {
-      res.status(500).send('Error connecting to the database');
-    } else {
-      res.send('Database connection successful!');
+  res.sendFile('index.html', { root: __dirname + '/public' });
+});
+
+app.post('/ask', (req, res) => {
+  const { question } = req.body;
+
+  if (!question) {
+    res.status(400).send('Please provide a question');
+    return;
+  }
+
+  // Store the question in the database (Assuming a 'questions' table)
+  connection.query(
+    'INSERT INTO questions (question_text) VALUES (?)',
+    [question],
+    (error, results, fields) => {
+      if (error) {
+        res.status(500).send('Error storing the question in the database');
+      } else {
+        res.send('Question submitted and stored in the database!');
+      }
     }
-  });
+  );
 });
 
 // Route to display employees
